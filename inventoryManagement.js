56a5506127c1cbaed9d438ec9b803cfeb2eacc17
Claudia Wavinya <mutungaclaudia5@gmail.com>
2026-09-26 02:07:17 +0300

    product inventory management lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,4 +1,10 @@
-// Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+function logFirstProduct() { console.log(products[0]);}
+function addProduct(productName) { products.push(productName);}
+function updateProductName(index,newName) {products[index] = newName ;}
+function removeLastProduct() {products.pop();}
+
+
 
 
 
